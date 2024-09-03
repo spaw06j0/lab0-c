22c54c3913c0dab8a128b32480bd3e6bbaf5eb6c
Jin <spaw06j0@gmail.com>
2024-09-04 01:59:09 +0800

    Implement q_swap(D-check), q_reverse

--- queue.c
+++ queue.c
@@ -181,12 +181,30 @@ bool q_delete_dup(struct list_head *head)
 /* Swap every two adjacent nodes */
 void q_swap(struct list_head *head)
 {
-    // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head || list_empty(head) || list_is_singular(head)) {
+        return;
+    }
+    struct list_head *node, *next;
+    list_for_each_safe (node, next, head) {
+        if (next == head) {
+            break;
+        }
+        list_move(next, node);
+        node = next->next;
+    }
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
-
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head)) {
+        return;
+    }
+    struct list_head *node, *next;
+    list_for_each_safe (node, next, head) {
+        list_move(node, head);
+    }
+}
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
 {
